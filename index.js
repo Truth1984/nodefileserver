e821902406749e227d905cc9318287d4d9e4d45f
awada <awada@localhost.localdomain>
2020-10-21 10:06:13 +0800

    add stat

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var config = require("./config");
 var readdir = require("./readdir");
 var app = express();
+var fs = require("fs");
+var paths = require("path");
 
 // convert to array
 if (!Array.isArray(config.root)) config.root = [config.root];
@@ -12,6 +14,19 @@ for (let i in config.root) config.root[i] = config.root[i].replace("~", process.
 for (let i of config.root) app.use(express.static(i));
 // sorted dir
 config._sortedRootDir = config.root.sort((a, b) => b.length - a.length);
+
+app.all("/stat/:path", (req, res, next) => {
+  let path = req.params.path;
+  for (let i of config.root) {
+    let fullpath = paths.join(i, path);
+    if (fs.existsSync(fullpath)) {
+      res.json(fs.statSync(fullpath));
+      return next();
+    }
+  }
+  res.json({ error: "file does not exist" });
+  return next();
+});
 
 app.all("/", (req, res, next) => {
   Promise.all(config.root.map((path) => readdir(path, config.ignore)))
@@ -24,8 +39,7 @@ app.all("/", (req, res, next) => {
             ""
           )
         );
-
-      data = data.filter((item) => config.filter.some((i) => new RegExp(i).test(item)));
+      if (config.filter.length > 0) data = data.filter((item) => config.filter.some((i) => new RegExp(i).test(item)));
       if (config.sort) data = data.sort();
       res.send(data);
     })
